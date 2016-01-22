f3798812cad59eb2e128a6da84fbb0c1f4148ddd
VLIGS <liudmila.veshneva@gmail.com>
2016-01-22 12:53:24 +0000

    added synchronisation stuff

---
+++ DesignPatterns/src/singleton/SingletonDoubleLocking.java
@@ -0,0 +1,19 @@
+package singleton;
+
+public class SingletonDoubleLocking {
+
+    private static SingletonDoubleLocking instance;
+    private SingletonDoubleLocking(){}
+
+    public static SingletonDoubleLocking getInstance(){
+        if(instance==null){
+            synchronized (SingletonDoubleLocking.class) {
+                if (instance == null) {
+                    instance = new SingletonDoubleLocking();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
