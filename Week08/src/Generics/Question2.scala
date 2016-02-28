85c5134dc4f2b570d0ac81b1cb419ddbdea399cc
VLIGS <liudmila.veshneva@gmail.com>
2016-02-28 16:21:53 +0000

    started on  Question2e

--- Week08/src/Generics/Question2.scala
+++ Week08/src/Generics/Question2.scala
@@ -2,13 +2,13 @@ package Generics
 
 sealed trait IntList {
 
-  def fold(end: Int, f: (Int, Int) => Int): Int =
+  def foldInt(end: Int, f: (Int, Int) => Int): Int =
     this match {
       case End => end
-      case Pair(hd, tl) => f(hd, tl.fold(end, f))
+      case Pair(hd, tl) => f(hd, tl.foldInt(end, f))
     }
 
-  def length1: Int = this.fold(0, (sum, _) => sum + 1)
+  def length1: Int = this.foldInt(0, (sum, _) => sum + 1)
 
   def length: Int =
     this match {
@@ -22,14 +22,14 @@ sealed trait IntList {
       case Pair(hd, tl) => Pair(hd * 2, tl.double)
     }
 
-  def product1: Int = this.fold(1, _ * _)
+  def product1: Int = this.foldInt(1, _ * _)
 
   def product: Int =
   this match {
   case End => 1
   case Pair(hd, tl) => hd * tl.product
   }
-  def sum1: Int = this.fold(0, _ + _)
+  def sum1: Int = this.foldInt(0, _ + _)
 
   def sum: Int =
   this match {
@@ -37,7 +37,6 @@ sealed trait IntList {
   case Pair(hd, tl) => hd + tl.sum
   }
 }
-
 case object End extends IntList
 case class Pair(head: Int, tail: IntList) extends IntList
 
@@ -53,9 +52,14 @@ object MyMain extends App {
 
 //Question 2c
 //It is more convenient to rewrite methods in terms of fold if they were implemented using pattern matching
-//rather than polymorphism.
+//rather than polymorphism. This is due to the fact that the methods need to be rewritten only once, rather
+//than as many times as there are case classes that extend trait if IntList was implemented using polymorphism.
+//Pattern Matching can be used here because we know all of the subclasses, so we can spell out all cases in match sequence
+
 
 //Question 2d
-//Double method does not fit the pattern that sum, product, and length have, in particular, base case does
-//not return Int but IntList
+//Double method does not fit the pattern that sum, product, and length have, in particular, it returns IntList
+//rather than just Int
+
+
 
---
+++ Week08/src/Generics/Question2e.scala
@@ -0,0 +1,57 @@
+
+package Generics
+
+sealed trait myList[A] {
+
+  def fold(end: A)(f: (A, A) => A): myList [A] =
+    this match {
+      case myEnd() => this
+      case myPair(hd, tl) => f(hd)( tl.fold(end, f))
+    }
+
+  def length1: myList[A] = this.fold(0)( (sum, _) => sum + 1)
+
+  def length: Int =
+    this match {
+      case myEnd() => 0
+      case myPair(hd, tl) => 1 + tl.length
+    }
+
+  def double: myList[A] =
+    this match {
+      case myEnd() => this
+      case myPair(hd, tl) => myPair(hd * 2, tl.double)
+    }
+
+  def product1: Int = this.fold(1)( _ * _)
+
+  def product: Int =
+    this match {
+      case myEnd() => 1
+      case myPair(hd, tl) => hd * tl.product
+    }
+  def sum1: Int = this.fold(0)( _ + _)
+
+  def sum: Int =
+    this match {
+      case myEnd() => 0
+      case myPair(hd, tl) => hd + tl.sum
+    }
+}
+case class myEnd() extends myList [Nothing]
+case class myPair[A](val head: A, val tail: myList[A]) extends myList [A]
+
+object MyMain extends App {
+  val myList: IntList = Pair(1, Pair(2, Pair(3, End)))
+  println(myList.length)
+  println(myList.length1)
+  println(myList.product)
+  println(myList.product1)
+  println(myList.sum)
+  println(myList.sum1)
+}
+
+
+
+
+
